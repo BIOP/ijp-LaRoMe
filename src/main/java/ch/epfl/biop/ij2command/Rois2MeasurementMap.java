b16891191fd33ad58ede8ac52bc8e3fc987e55dd
romainGuiet <romain.guiet@epfl.ch>
2021-02-05 08:37:04 +0100

    adds "track" as a possible measurement

--- src/main/java/ch/epfl/biop/ij2command/Rois2MeasurementMap.java
+++ src/main/java/ch/epfl/biop/ij2command/Rois2MeasurementMap.java
@@ -13,6 +13,8 @@ import org.scijava.plugin.Plugin;
 
 import ij.process.ImageStatistics;
 
+import static javax.xml.bind.DatatypeConverter.parseFloat;
+
 /**
  *
  * <p>
@@ -32,7 +34,7 @@ public class Rois2MeasurementMap implements Command {
     @Parameter
     RoiManager rm ;
 
-    @Parameter ( label="Measure", choices = {"Area", "Angle", "AngleVert","AR", "Circ.", "Major","Minor","Mean","Median","Mode","Min","Max", "Perim."})
+    @Parameter ( label="Measure", choices = {"Area", "Angle", "AngleVert","AR", "Circ.", "Major","Minor","Mean","Median","Mode","Min","Max", "Perim.","Track"})
     String column_name;
 
     @Override
@@ -46,52 +48,85 @@ public class Rois2MeasurementMap implements Command {
 
     private ImagePlus R2M(ImagePlus imp ){
 
+        // duplicate the imp for the task
         ImagePlus imp2 = imp.duplicate();
-        // duplicate the processor for the task
-        // as a 32-bit (because measurements can be float , or negative)
+        // check if it's a stack
+        int stackN = imp2.getImageStackSize();
+        boolean isStack = false ;
+        if (stackN > 1 ) isStack = true ;
+        // reset imp2
+        for (int i = 0; i < stackN ; i++) {
+            imp2.getStack().getProcessor(i + 1).setValue(0.0);
+            imp2.getStack().getProcessor(i + 1).fill();
+        }
+        // convert to 32-bit (because measurements can be float , or negative)
         IJ.run(imp2, "32-bit", "");
-        //imp2.getProcessor.setValue(0.0);
-        //imp2.getProcessor.fill(); // reset image
-
-        boolean isStack = false;
-        if (imp.getImageStackSize() > 1) isStack = true;
+        // initiate the filling value
+        double filling_value = 0.0;
 
         Roi[] rois = rm.getRoisAsArray()  ;
         for (int i = 0; i < rois.length; i++) {
+            //set the position in the stack if necessary
             if ( isStack )  imp2.setPosition( rois[i].getPosition() );
+            // and set the ROI
             imp2.setRoi( rois[i]);
+            // so we can get Stats
             ImageStatistics ip_stats = imp2.getProcessor().getStatistics() ;
-            double filling_value = 0.0;
-
+            System.out.println( ip_stats);
+            // from user choice
             switch (column_name) {
-                case "Area" :   filling_value = ip_stats.area;
+                case "Area" :
+                    filling_value = ip_stats.area;
                     break;
-                case "Angle" :  filling_value = ip_stats.angle;
+                case "Angle" :
+                    filling_value = ip_stats.angle;
                     break;
                 // the Angle measure is based on horizontal,
                 // the AngleVert measure is based on vertical (substracting 90 )
-                case "AngleVert" :  filling_value = ip_stats.angle - 90 ;
+                case "AngleVert" :
+                    filling_value = ip_stats.angle - 90 ;
                     break;
-                case "AR" :     filling_value = ip_stats.major / ip_stats.minor;
+                case "AR" :
+                    filling_value = ip_stats.major / ip_stats.minor;
                     break;
                 case "Circ." :   // 4 x  pi x area / perimeter^2
                     filling_value = 4*Math.PI * ip_stats.area / Math.pow(rois[i].getLength() , 2 );
                     break;
-                case "Major":   filling_value = ip_stats.major;
+                case "Major":
+                    filling_value = ip_stats.major;
                     break;
-                case "Minor" :  filling_value = ip_stats.minor;
+                case "Minor" :
+                    filling_value = ip_stats.minor;
                     break;
-                case "Mean" :   filling_value = ip_stats.mean;
+                case "Mean" :
+                    filling_value = ip_stats.mean;
                     break;
-                case "Median" : filling_value = ip_stats.median;
+                case "Median" :
+                    filling_value = ip_stats.median;
                     break;
-                case "Mode" :   filling_value = ip_stats.mode;
+                case "Mode" :
+                    filling_value = ip_stats.mode;
                     break;
-                case "Min" :    filling_value = ip_stats.min;
+                case "Min" :
+                    filling_value = ip_stats.min;
                     break;
-                case "Max" :    filling_value = ip_stats.max;
+                case "Max" :
+                    filling_value = ip_stats.max;
                     break;
-                case "Perim.":   filling_value = rois[i].getLength();
+                case "Perim.":
+                    filling_value = rois[i].getLength();
+                    break;
+                case "Track":
+                    // roi_name follows the model Track-0001:Frame-0001 ...
+                    // which corresponds to an output from a custom script making use of TrackMate, to link rois
+                    // value will be the number of the track, hijacking Rois2Measurements to make a  Rois2Labels
+                    String roi_name = rois[i].getName();
+                    if (roi_name.matches("Track-.*")) {
+                        String[] splitted_roi_name = roi_name.split(":");
+                        filling_value = parseFloat( splitted_roi_name[0].split("-")[1] );
+                    }else{ // orphan ROIs won't be filled
+                        filling_value = 0.0;
+                    }
                     break;
             }
 
@@ -99,7 +134,7 @@ public class Rois2MeasurementMap implements Command {
             imp2.getProcessor().fill( rois[i] );
 
         }
-
+        imp2.setTitle(column_name +"_Image");
         return imp2;
         //ImagePlus imp2 = new ImagePlus(column_name +"_Image" , ip_result );
         //imp2.show();
@@ -125,6 +160,7 @@ public class Rois2MeasurementMap implements Command {
         // create the ImageJ application context with all available services
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
+        RoiManager rm = RoiManager.getRoiManager();
 
         Boolean test_with_single_image = true ;
         ImagePlus imp = new ImagePlus();
@@ -146,9 +182,6 @@ public class Rois2MeasurementMap implements Command {
         }
 
         IJ.run(imp, "Label image to ROIs", "");
-
-        RoiManager rm = RoiManager.getRoiManager();
-
         ij.command().run(Rois2MeasurementMap.class, true, "column_name","Area");
     }
 }
