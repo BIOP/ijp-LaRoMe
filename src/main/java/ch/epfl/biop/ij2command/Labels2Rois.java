fdd7ebb64a39e5ad0237c782ce570b8951ec058e
oburri <olivier.burri@gmail.com>
2023-05-03 13:05:35 +0200

    update license blurbs

--- src/main/java/ch/epfl/biop/ij2command/Labels2Rois.java
+++ src/main/java/ch/epfl/biop/ij2command/Labels2Rois.java
@@ -2,7 +2,7 @@
  * #%L
  * LaRoMe - BIOP - EPFL
  * %%
- * Copyright (C) 2019 - 2022 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
+ * Copyright (C) 2019 - 2023 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, BioImaging And Optics Platform (BIOP)
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
